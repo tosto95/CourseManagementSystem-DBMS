1926273b9d253663936c34f4b37b977156b6cc0a
sohands <sohandesarkar@gmail.com>
2016-03-02 16:17:06 +0530

    Commit - SOhan minor changes

--- People/models.py
+++ People/models.py
@@ -7,41 +7,41 @@ from django.db import models
 
 class Student(models.Model):
 	studentID = models.AutoField(primary_key = True)
-	firstName = models.CharField(max_length = 100)
-	lastName = models.CharField(max_length = 100)
-	dob = models.DateTimeField('Date of Birth')
+	firstName = models.CharField('First Name',max_length = 100)
+	lastName = models.CharField('Last Name',max_length = 100)
+	dob = models.DateField('Date of Birth')
 	age = models.IntegerField()
-	studentUserName = models.CharField(max_length = 100)
-	studentPassword = models.CharField(max_length = 100)
+	studentUserName = models.CharField('Login Username',max_length = 100)
+	studentPassword = models.CharField('Login Password',max_length = 100)
 	def __str__(self):
 		return self.studentID
 
 class Faculty(models.Model):
 	facultyID = models.AutoField(primary_key = True)
-	firstName = models.CharField(max_length = 100)
-	lastName = models.CharField(max_length = 100)
-	dob = models.DateTimeField('Date of Birth')
+	firstName = models.CharField('First Name',max_length = 100)
+	lastName = models.CharField('Last Name',max_length = 100)
+	dob = models.DateField('Date of Birth')
 	age = models.IntegerField()
-	facultyUserName = models.CharField(max_length = 100)
-	facultyPassword = models.CharField(max_length = 100)
+	facultyUserName = models.CharField('Login Username',max_length = 100)
+	facultyPassword = models.CharField('Login Password',max_length = 100)
 	def __str__(self):
 		return self.facultyID
 
 class Parent(models.Model):
 	parentID = models.AutoField(primary_key = True)
-	firstName = models.CharField(max_length = 100)
-	lastName = models.CharField(max_length = 100)
-	parentUserName = models.CharField(max_length = 100)
-	parentPassword = models.CharField(max_length = 100)
+	firstName = models.CharField('First Name',max_length = 100)
+	lastName = models.CharField('Last Name',max_length = 100)
+	parentUserName = models.CharField('Login Username',max_length = 100)
+	parentPassword = models.CharField('Login Password',max_length = 100)
 	def __str__(self):
 		return self.parentID
 
 class Admin(models.Model):
 	adminID = models.AutoField(primary_key = True)
-	firstName = models.CharField(max_length = 100)
-	lastName = models.CharField(max_length = 100)
-	adminUserName = models.CharField(max_length = 100)
-	adminPassword = models.CharField(max_length = 100)
+	firstName = models.CharField('First Name',max_length = 100)
+	lastName = models.CharField('Last Name',max_length = 100)
+	adminUserName = models.CharField('Login Username',max_length = 100)
+	adminPassword = models.CharField('Login Password',max_length = 100)
 	def __str__(self):
 		return self.adminID
 
